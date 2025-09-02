849de33f5b4d233bce7654de720eb7aa35bf96be
Ritesh-Kumar546 <ritesh.kumar.24cse@bmu.edu.in>
2025-09-02 10:57:53 +0530

    intial commit

---
+++ project-001/Readme.md
@@ -0,0 +1,14 @@
+You need to build a CLI (Command Line Interface) application in Node.js that:
+
+Takes two arguments from the command line:
+
+a URL (e.g. https://example.com)
+
+a CSS selector (e.g. "h1")
+
+Fetches the HTML content of the page at that URL. Using Axios.
+
+Uses cheerio (a jQuery-like library for Node) to parse the HTML.
+
+Prints the text content of the element(s) that match the selector.
+
---
+++ project-001/index.js
@@ -0,0 +1,87 @@
+
+const readline = require("readline");
+const cheerio = require('cheerio');
+const axios = require('axios');
+const { styleText } = require('node:util');
+const PORT = 5000;
+
+
+// const server = http.createServer((req, res) =>{
+//     res.writeHead(200, {'Content-Type':'text/plain'});
+
+//     if (req.method === 'GET' && req.url === 'http://localhost:5000/') {
+
+//     }
+//     res.end("Hello! World");
+// });
+
+
+// server.listen(PORT, ()=>{
+//     // console.log(`server running at http://localhost:${PORT}`);
+// })
+
+// Create an interface for input and output
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let lines = [];
+
+rl.question('Enter a URL:',(firstline)=>{
+    rl.question('Enter the CSS Selector:',(secondline)=>{
+        lines.push(firstline);
+        lines.push(secondline);
+        console.log(`u entered URL:${firstline} and CSS Selector:${secondline}`);
+        // console.log(lines);
+        const URL = lines[0];
+        const Selector = lines[1];
+        Scrape(URL, Selector);
+        rl.close();
+    })
+});
+
+
+// axios.get("https://www.geeksforgeeks.org/node-js/rest-api-introduction/",{
+//     headers: {
+//         "Accept":"text/html"
+//     }
+// })
+// .then((res) =>{
+//     html =res.data;
+//     const $ = cheerio.load(html);
+//     console.log($('h1').text());
+// }).catch((err) =>{
+//     console.log(err);
+// })
+
+
+async function Scrape(URL, Selector) {
+    try {
+        const res = await axios.get(`${URL}`,{
+            headers: {
+                "Accept":"text/html"
+    }})
+    const $ = cheerio.load(res.data);
+    const text = $(`${Selector}`).text();
+    console.log(styleText(['red','bold'],text));
+
+    } catch(err) {
+        console.log("Error:",err);
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
